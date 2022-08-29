e719df1ca196b7c220c6c4a1a831c08863940d9e
Pawan Kumar <pawanandaman@gmail.com>
2022-08-29 22:09:33 +0530

    Added Array

---
+++ array.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+class Array
+{
+    int* arr;
+    int size;
+
+    public:
+        Array();
+        Array(int size);
+        void take_input();
+        void display();
+        Array add(Array temp);
+};
